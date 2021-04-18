2d173f7496904688bbba33ab607cdf2ffc165bf6
paulohfev <phfernandesev@gmail.com>
2021-04-17 22:27:33 -0300

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <header>
-        Matching game
-      </header>
+    <div className='container'>
+      <section className='cards-container'>
+        <div className='cards'>
+          <div className='card'>1</div>
+          <div className='card'>2</div>
+          <div className='card'>3</div>
+          <div className='card'>4</div>
+          <div className='card'>5</div>
+          <div className='card'>6</div>
+          <div className='card'>7</div>
+          <div className='card'>8</div>
+          <div className='card'>9</div>
+          <div className='card'>10</div>
+          <div className='card'>11</div>
+          <div className='card'>12</div>
+        </div>
+      </section>
     </div>
   );
 }
